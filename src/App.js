8305652633ccc5570f762bff1c6396a0ddcda5e5
terryjung0244 <terryjung0244@gmail.com>
2022-09-10 16:19:34 -0400

    Created useEffect and the current location's api call updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      console.log('currentLocation', lat, lon);
+      getWeahterByCurrentLocation(lat, lon);
+    });
+
+    const getWeahterByCurrentLocation = async (lat, lon) => {
+      let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=4b917ff881afce42f8453ca1be779805`
+      let response = await fetch(url)
+      let data = await response.json();
+      console.log('data', data)
+    }
+    
+  };
+
+  useEffect(() => {
+    getCurrentLocation()  
+  }, [])
+
   return (
     <div>
       App
@@ -9,3 +31,4 @@ const App = () => {
 }
 
 export default App
+  
